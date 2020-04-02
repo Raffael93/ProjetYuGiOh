62ed77bccbbf365173330f8c2f8ddd033b7087b3
Raffael93 <raffael.poinsot@gmail.com>
2020-04-02 18:49:40 +0200

    first commit and first page activity

---
+++ app/src/main/java/com/example/projetyugioh/Activity2.java
@@ -0,0 +1,40 @@
+package com.example.projetyugioh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class Activity2 extends AppCompatActivity {
+
+    private Button buttonRetour;
+    private PDFView pdfview;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        pdfview = findViewById(R.id.pdfview);
+        pdfview.fromAsset("rule.pdf").load();
+
+
+        buttonRetour = findViewById(R.id.buttonRetour);
+        buttonRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                previous();
+            }
+        });
+    }
+
+    public void previous(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/projetyugioh/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.projetyugioh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRule();
+            }
+        });
+    }
+
+
+    public void openRule(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
+
+
+}

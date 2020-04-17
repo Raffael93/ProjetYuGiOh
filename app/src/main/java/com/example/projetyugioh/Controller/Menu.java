8715bdffb7b913e3dff4973baf3ae0f9692cc799
Raffael93 <raffael.poinsot@gmail.com>
2020-04-17 14:59:55 +0200

    SUBARASHI, still data cache to do

---
+++ app/src/main/java/com/example/projetyugioh/Controller/Menu.java
@@ -0,0 +1,41 @@
+package com.example.projetyugioh.Controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+import com.example.projetyugioh.R;
+
+public class Menu extends AppCompatActivity {
+
+    private VideoView videoView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_menu);
+
+        getSupportActionBar().hide();
+
+        videoView = findViewById(R.id.videoView);
+
+        Uri video = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.video);
+        videoView.setVideoURI(video);
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                if(isFinishing())
+                    return;
+                startActivity(new Intent(Menu.this,MainActivity.class));
+                finish();
+            }
+        });
+        videoView.start();
+    }
+}
